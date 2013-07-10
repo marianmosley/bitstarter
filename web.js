15272f1e186a2d783fd0a3ead4974d11bea8d474
marianmosley <marian.mosley@gmail.com>
2013-07-10 19:51:01 +0000

    read from file

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = new Buffer(256);
+  buffer = fs.readFileSync("index.html","utf-8");
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
